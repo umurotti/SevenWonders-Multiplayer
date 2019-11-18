3663d0facd93f99ab6519524791c8409e2ee79cc
Ömer Faruk Kürklü <32766041+OmerFarukKurklu@users.noreply.github.com>
2019-11-19 01:12:07 +0300

    Game engine implementations
    Action.java, Card.java, Cost.java. Files might not have been changed.

--- backhand/src/main/java/model/Action.java
+++ backhand/src/main/java/model/Action.java
@@ -1,24 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package model;
-
-import java.util.Map;
+import java.util.HashMap;
+import java.util.Objects;
 
 /**
- *
- * @author umur
+ * @author AkinParkan, OmerFarukKurklu
+ * @version 0.2
  */
 public class Action {
     private int choice;
-    private Map<String,Integer> leftTrade;
-    private Map<String,Integer> rightTrade;
+    private HashMap<String,Integer> leftTrade;
+    private HashMap<String,Integer> rightTrade;
     private int cardNo;
     private String wonderID;
 
-    public Action(int choice, Map<String, Integer> leftTrade, Map<String, Integer> rightTrade, int cardNo, String wonderID) {
+    public Action() {
+    }
+
+    public Action(int choice, HashMap<String,Integer> leftTrade, HashMap<String,Integer> rightTrade, int cardNo, String wonderID) {
         this.choice = choice;
         this.leftTrade = leftTrade;
         this.rightTrade = rightTrade;
@@ -27,31 +24,31 @@ public class Action {
     }
 
     public int getChoice() {
-        return choice;
+        return this.choice;
     }
 
     public void setChoice(int choice) {
         this.choice = choice;
     }
 
-    public Map<String, Integer> getLeftTrade() {
-        return leftTrade;
+    public HashMap<String,Integer> getLeftTrade() {
+        return this.leftTrade;
     }
 
-    public void setLeftTrade(Map<String, Integer> leftTrade) {
+    public void setLeftTrade(HashMap<String,Integer> leftTrade) {
         this.leftTrade = leftTrade;
     }
 
-    public Map<String, Integer> getRightTrade() {
-        return rightTrade;
+    public HashMap<String,Integer> getRightTrade() {
+        return this.rightTrade;
     }
 
-    public void setRightTrade(Map<String, Integer> rightTrade) {
+    public void setRightTrade(HashMap<String,Integer> rightTrade) {
         this.rightTrade = rightTrade;
     }
 
     public int getCardNo() {
-        return cardNo;
+        return this.cardNo;
     }
 
     public void setCardNo(int cardNo) {
@@ -59,10 +56,63 @@ public class Action {
     }
 
     public String getWonderID() {
-        return wonderID;
+        return this.wonderID;
     }
 
     public void setWonderID(String wonderID) {
         this.wonderID = wonderID;
     }
+
+    public Action choice(int choice) {
+        this.choice = choice;
+        return this;
+    }
+
+    public Action leftTrade(HashMap<String,Integer> leftTrade) {
+        this.leftTrade = leftTrade;
+        return this;
+    }
+
+    public Action rightTrade(HashMap<String,Integer> rightTrade) {
+        this.rightTrade = rightTrade;
+        return this;
+    }
+
+    public Action cardNo(int cardNo) {
+        this.cardNo = cardNo;
+        return this;
+    }
+
+    public Action wonderID(String wonderID) {
+        this.wonderID = wonderID;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Action)) {
+            return false;
+        }
+        Action action = (Action) o;
+        return choice == action.choice && Objects.equals(leftTrade, action.leftTrade) && Objects.equals(rightTrade, action.rightTrade) && cardNo == action.cardNo && Objects.equals(wonderID, action.wonderID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(choice, leftTrade, rightTrade, cardNo, wonderID);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " choice='" + getChoice() + "'" +
+            ", leftTrade='" + getLeftTrade() + "'" +
+            ", rightTrade='" + getRightTrade() + "'" +
+            ", cardNo='" + getCardNo() + "'" +
+            ", wonderID='" + getWonderID() + "'" +
+            "}";
+    }
+
 }
--- backhand/src/main/java/model/Card.java
+++ backhand/src/main/java/model/Card.java
@@ -3,93 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model;
 
-import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.Objects;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
- *
- * @author akın, omer, umur
+ * @author AkinParkan, OmerFarukKurklu
+ * @version 0.2
  */
-public class Card implements Serializable {
-private Cost cost;
+
+// This is not the Interface Class Card. This is basically the SimpleCard class for simplicity in demo 1.
+public class Card {
+    private Cost cost;
     private String color;
     private String name;
-    private String freeBuildings;
-    private int minPlayerNo;
-    private HashMap<String,Integer> benefits;
+    private String freeBuilding;
+    private HashMap<String,Integer> benefits; // String: name of the resource, Integer: amount of the resource
+    private int minNoOfPlayer;
 
-
-    public Card(Cost cost, String color, String name, String freeBuildings, int minNoOfPlayer, HashMap<String, Integer> benefits) {
-        this.cost = cost;
-        this.color = color;
-        this.name = name;
-        this.freeBuildings = "";
-        this.minPlayerNo = minNoOfPlayer;
-        this.benefits = benefits;
-    }
 
     public Card() {
-        benefits = new HashMap<>();
     }
 
-
-
-
-    public int getMinPlayerNo() {
-        return minPlayerNo;
-    }
-
-
-    public Map<String, Integer> getBenefits() {
-        return benefits;
-    }
-
-    public void setBenefits(HashMap<String, Integer> benefits) {
-        this.benefits = benefits;
-    }
-
-    public void setMinPlayerNo(int minPlayerNo) {
-        this.minPlayerNo = minPlayerNo;
-    }
-
-    public Cost getCost() {
-        return cost;
-    }
-
-    public void setCost(Cost cost) {
+    public Card(Cost cost, String color, String name, String freeBuilding, HashMap<String,Integer> benefits, int minNoOfPlayer) {
         this.cost = cost;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
         this.color = color;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public String getFreeBuildings() {
-        return freeBuildings;
-    }
-    public void setFreeBuildings(String freeBuildings) {
-        this.freeBuildings = freeBuildings;
+        this.freeBuilding = freeBuilding;
+        this.benefits = benefits;
+        this.minNoOfPlayer = minNoOfPlayer;
     }
 
     public void play(WonderBoard wb, String selection)
-    {
+    {   
         // Update te sources of the WonderBoard.
         HashMap<String, Integer> wbSources = wb.getSources();
         for(String sourceName : benefits.keySet()) {
@@ -105,8 +51,109 @@ private Cost cost;
         wb.setBuiltCards(wbCards);
     }
 
+    public Cost getCost() {
+        return this.cost;
+    }
+
+    public void setCost(Cost cost) {
+        this.cost = cost;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFreeBuilding() {
+        return this.freeBuilding;
+    }
+
+    public void setFreeBuilding(String freeBuilding) {
+        this.freeBuilding = freeBuilding;
+    }
+
+    public HashMap<String,Integer> getBenefits() {
+        return this.benefits;
+    }
+
+    public void setBenefits(HashMap<String,Integer> benefits) {
+        this.benefits = benefits;
+    }
+
+    public int getMinNoOfPlayer() {
+        return this.minNoOfPlayer;
+    }
+
+    public void setMinNoOfPlayer(int minNoOfPlayer) {
+        this.minNoOfPlayer = minNoOfPlayer;
+    }
+
+    public Card cost(Cost cost) {
+        this.cost = cost;
+        return this;
+    }
+
+    public Card color(String color) {
+        this.color = color;
+        return this;
+    }
+
+    public Card name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Card freeBuildings(String freeBuilding) {
+        this.freeBuilding = freeBuilding;
+        return this;
+    }
+
+    public Card benefits(HashMap<String,Integer> benefits) {
+        this.benefits = benefits;
+        return this;
+    }
+
+    public Card minNoOfPlayer(int minNoOfPlayer) {
+        this.minNoOfPlayer = minNoOfPlayer;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Card)) {
+            return false;
+        }
+        Card card = (Card) o;
+        return Objects.equals(cost, card.cost) && Objects.equals(color, card.color) && Objects.equals(name, card.name) && Objects.equals(freeBuilding, card.freeBuilding) && Objects.equals(benefits, card.benefits) && minNoOfPlayer == card.minNoOfPlayer;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cost, color, name, freeBuilding, benefits, minNoOfPlayer);
+    }
+
     @Override
     public String toString() {
-        return "Card{" + "cost=" + cost + ", color=" + color + ", name=" + name +  ", benefits=" + benefits + ", minNoOfPlayer=" + minPlayerNo + '}';
+        return "{" +
+            " cost='" + getCost() + "'" +
+            ", color='" + getColor() + "'" +
+            ", name='" + getName() + "'" +
+            ", freeBuildings='" + getFreeBuilding() + "'" +
+            ", benefits='" + getBenefits() + "'" +
+            ", minNoOfPlayer='" + getMinNoOfPlayer() + "'" +
+            "}";
     }
 }
