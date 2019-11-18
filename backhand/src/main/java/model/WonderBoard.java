c6378a9aa7a35c34436d8e22f6324df1a0752983
Ömer Faruk Kürklü <32766041+OmerFarukKurklu@users.noreply.github.com>
2019-11-19 01:13:09 +0300

    Add files via upload

--- backhand/src/main/java/model/WonderBoard.java
+++ backhand/src/main/java/model/WonderBoard.java
@@ -1,19 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package model;
-
 import java.util.HashMap;
 import java.util.Objects;
 
 
-/**
+/** 
  * @author: OmerFarukKurklu
  * @version: 0.1
- */
-public class WonderBoard {
+*/
+class WonderBoard {
     private WonderBoard leftNeighbor;
     private WonderBoard rightNeighbor;
     private HashMap<String,Integer> sources; // String: Name of the resource, Integer: Amount of the resource
@@ -27,6 +20,7 @@ public class WonderBoard {
     private HashMap<String, Integer> rightDiscount; // String: Name of the resource, Integer: price of the resource from left neighbor
     private int[] militaryTokens;
     private int defeatTokens;
+    private int handNo;
 
     public WonderBoard() {
         // militaryTokens[0]: 1st age military victories, militaryTokens[1]: 2nd, militaryTokens[2]: 3rd.
@@ -75,62 +69,7 @@ public class WonderBoard {
         }};
     }
 
-    public WonderBoard( String name) {
-        this.name = name;
-        // militaryTokens[0]: 1st age military victories, militaryTokens[1]: 2nd, militaryTokens[2]: 3rd.
-        militaryTokens = new int[3];
-
-        // stageCosts[0]: 1st stage cost, stageCosts[1]: 2nd, stageCosts[2]: 3rd.
-        stageCosts = new Cost[3];
-
-        sources = new HashMap<String,Integer>()
-        {{
-            put("wood", 1);
-            put("stone", 3);
-            put("clay", 2);
-            put("ore", 7);
-            put("loom",0);
-            put("papyrus", 0);
-            put("glass", 1);
-            put("compass", 3);
-            put("tablet", 2);
-            put("gear", 7);
-            put("coin", 0);
-            put("shield", 0);
-            put("victoryPoint", 0);
-        }};
-        
-        builtCards  = new HashMap<String,Card>()
-        {{
-            put("altar", new Card(new Cost(new HashMap<String, Integer>()), "red", "altar", "", 3, sources));
-        }};
-
-        leftDiscount = new HashMap<String, Integer>()
-        {{
-            put("wood", 2);
-            put("stone", 2);
-            put("clay", 2);
-            put("ore", 2);
-            put("loom", 2);
-            put("papyrus", 2);
-            put("glass", 2);
-        }};
-        rightDiscount = new HashMap<String, Integer>()
-        {{
-            put("wood", 2);
-            put("stone", 2);
-            put("clay", 2);
-            put("ore", 2);
-            put("loom", 2);
-            put("papyrus", 2);
-            put("glass", 2);
-        }};
-    }
-
-    public WonderBoard(WonderBoard leftNeighbor, WonderBoard rightNeighbor, HashMap<String,Integer> sources,
-                       Cost[] stageCosts, int currentStage, int diceValue, String name, HashMap<String,Card> builtCards,
-                       Action lockedAction, HashMap<String,Integer> leftDiscount, HashMap<String,Integer> rightDiscount,
-                       int[] militaryTokens, int defeatTokens) {
+    public WonderBoard(WonderBoard leftNeighbor, WonderBoard rightNeighbor, HashMap<String,Integer> sources, Cost[] stageCosts, int currentStage, int diceValue, String name, HashMap<String,Card> builtCards, Action lockedAction, HashMap<String,Integer> leftDiscount, HashMap<String,Integer> rightDiscount, int[] militaryTokens, int defeatTokens, int handNo) {
         this.leftNeighbor = leftNeighbor;
         this.rightNeighbor = rightNeighbor;
         this.sources = sources;
@@ -144,6 +83,7 @@ public class WonderBoard {
         this.rightDiscount = rightDiscount;
         this.militaryTokens = militaryTokens;
         this.defeatTokens = defeatTokens;
+        this.handNo = handNo;
     }
 
     public WonderBoard getLeftNeighbor() {
@@ -250,6 +190,14 @@ public class WonderBoard {
         this.defeatTokens = defeatTokens;
     }
 
+    public int getHandNo() {
+        return this.handNo;
+    }
+
+    public void setHandNo(int handNo) {
+        this.handNo = handNo;
+    }
+
     public WonderBoard leftNeighbor(WonderBoard leftNeighbor) {
         this.leftNeighbor = leftNeighbor;
         return this;
@@ -315,6 +263,11 @@ public class WonderBoard {
         return this;
     }
 
+    public WonderBoard handNo(int handNo) {
+        this.handNo = handNo;
+        return this;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == this)
@@ -323,30 +276,32 @@ public class WonderBoard {
             return false;
         }
         WonderBoard wonderBoard = (WonderBoard) o;
-        return Objects.equals(leftNeighbor, wonderBoard.leftNeighbor) && Objects.equals(rightNeighbor, wonderBoard.rightNeighbor) && Objects.equals(sources, wonderBoard.sources) && Objects.equals(stageCosts, wonderBoard.stageCosts) && currentStage == wonderBoard.currentStage && diceValue == wonderBoard.diceValue && Objects.equals(name, wonderBoard.name) && Objects.equals(builtCards, wonderBoard.builtCards) && Objects.equals(lockedAction, wonderBoard.lockedAction) && Objects.equals(leftDiscount, wonderBoard.leftDiscount) && Objects.equals(rightDiscount, wonderBoard.rightDiscount) && Objects.equals(militaryTokens, wonderBoard.militaryTokens) && defeatTokens == wonderBoard.defeatTokens;
+        return Objects.equals(leftNeighbor, wonderBoard.leftNeighbor) && Objects.equals(rightNeighbor, wonderBoard.rightNeighbor) && Objects.equals(sources, wonderBoard.sources) && Objects.equals(stageCosts, wonderBoard.stageCosts) && currentStage == wonderBoard.currentStage && diceValue == wonderBoard.diceValue && Objects.equals(name, wonderBoard.name) && Objects.equals(builtCards, wonderBoard.builtCards) && Objects.equals(lockedAction, wonderBoard.lockedAction) && Objects.equals(leftDiscount, wonderBoard.leftDiscount) && Objects.equals(rightDiscount, wonderBoard.rightDiscount) && Objects.equals(militaryTokens, wonderBoard.militaryTokens) && defeatTokens == wonderBoard.defeatTokens && handNo == wonderBoard.handNo;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(leftNeighbor, rightNeighbor, sources, stageCosts, currentStage, diceValue, name, builtCards, lockedAction, leftDiscount, rightDiscount, militaryTokens, defeatTokens);
+        return Objects.hash(leftNeighbor, rightNeighbor, sources, stageCosts, currentStage, diceValue, name, builtCards, lockedAction, leftDiscount, rightDiscount, militaryTokens, defeatTokens, handNo);
     }
 
     @Override
     public String toString() {
         return "{" +
-                " leftNeighbor='" + getLeftNeighbor() + "'" +
-                ", rightNeighbor='" + getRightNeighbor() + "'" +
-                ", sources='" + getSources() + "'" +
-                ", stageCosts='" + getStageCosts() + "'" +
-                ", currentStage='" + getCurrentStage() + "'" +
-                ", diceValue='" + getDiceValue() + "'" +
-                ", name='" + getName() + "'" +
-                ", builtCards='" + getBuiltCards() + "'" +
-                ", lockedAction='" + getLockedAction() + "'" +
-                ", leftDiscount='" + getLeftDiscount() + "'" +
-                ", rightDiscount='" + getRightDiscount() + "'" +
-                ", militaryTokens='" + getMilitaryTokens() + "'" +
-                ", defeatTokens='" + getDefeatTokens() + "'" +
-                "}";
+            " leftNeighbor='" + getLeftNeighbor() + "'" +
+            ", rightNeighbor='" + getRightNeighbor() + "'" +
+            ", sources='" + getSources() + "'" +
+            ", stageCosts='" + getStageCosts() + "'" +
+            ", currentStage='" + getCurrentStage() + "'" +
+            ", diceValue='" + getDiceValue() + "'" +
+            ", name='" + getName() + "'" +
+            ", builtCards='" + getBuiltCards() + "'" +
+            ", lockedAction='" + getLockedAction() + "'" +
+            ", leftDiscount='" + getLeftDiscount() + "'" +
+            ", rightDiscount='" + getRightDiscount() + "'" +
+            ", militaryTokens='" + getMilitaryTokens() + "'" +
+            ", defeatTokens='" + getDefeatTokens() + "'" +
+            ", handNo='" + getHandNo() + "'" +
+            "}";
     }
+
 }
